e015a05aff34cbf0258ac9a41164483326a0c78f
Priyanshu Bhandari <technopriyu@gmail.com>
2021-08-10 16:47:22 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose
 
 // 4) START SERVER
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
   console.log(`App running on port ${port}...`)
 );
